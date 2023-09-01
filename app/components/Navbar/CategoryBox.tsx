4cd4d7529e857c363e0525d4f74d6402ea61c383
Prasham1710 <jainprasham17@gmail.com>
2023-09-02 05:12:37 +0530

    Categorie completed

--- app/components/Navbar/CategoryBox.tsx
+++ app/components/Navbar/CategoryBox.tsx
@@ -1,41 +1,70 @@
+'use client';
 import { useRouter, useSearchParams } from 'next/navigation';
 import {useCallback} from 'react'
 import { IconType } from 'react-icons'
-import qs from 'query-string'
+import qs from 'query-string';
 interface CategoryBoxProps{
     icon: IconType;
     label: string;
     selected?: boolean;
 }
 const CategoryBox: React.FC<CategoryBoxProps> = ({
-    icon : Icon ,
-    label,
-    selected
+  icon: Icon,
+  label,
+  selected,
 }) => {
-    const router = useRouter();
-    const params = useSearchParams();
-    const handleClick = useCallback(
-      () => {
-        let currentQuery = {};
+  const router = useRouter();
+  const params = useSearchParams();
 
-        if (params)
-        {
-            currentQuery = qs.parse(params.toString());
-      }
-    },
-      [],
-    )
+  const handleClick = useCallback(() => {
+    let currentQuery = {};
+    
+    if (params) {
+      currentQuery = qs.parse(params.toString())
+    }
+
+    const updatedQuery: any = {
+      ...currentQuery,
+      category: label
+    }
+
+    if (params?.get('category') === label) {
+      delete updatedQuery.category;
+    }
+
+    const url = qs.stringifyUrl({
+      url: '/',
+      query: updatedQuery
+    }, { skipNull: true });
+
+    router.push(url);
+  }, [label, router, params]);
+
     
   return (
-    <div className={`flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transtition cursor-pointer
-    ${selected ? 'boder-b-neutral-800' : 'border-transparent'}
-    ${selected ? 'text-neutral-800' : 'text-neutral-500'}`}> 
-    <Icon size={26}/>
-    <div className='font-medium text-sm'>
+    <div
+      onClick={handleClick}
+      className={`
+        flex 
+        flex-col 
+        items-center 
+        justify-center 
+        gap-2
+        p-3
+        border-b-2
+        hover:text-neutral-800
+        transition
+        cursor-pointer
+        ${selected ? 'border-b-neutral-800' : 'border-transparent'}
+        ${selected ? 'text-neutral-800' : 'text-neutral-500'}
+      `}
+    >
+      <Icon size={26} />
+      <div className="font-medium text-sm">
         {label}
+      </div>
     </div>
-    </div>
-  )
+   );
 }
 
 export default CategoryBox
